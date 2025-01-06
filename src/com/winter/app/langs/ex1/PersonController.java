ccb614e373dc97676ea249d36692d16d7cc7ac86
merandie <kimdaeki@gmail.com>
2025-01-06 17:14:43 +0900

    1

--- src/com/winter/app/langs/ex1/PersonController.java
+++ src/com/winter/app/langs/ex1/PersonController.java
@@ -6,7 +6,9 @@ public class PersonController {
 	public void start() {
 		//1. 주소록 초기화
 		//2. 주소록 출력
-		//3. 검색(이름)
+		//3. 검색(이름, PathInfo.find())
+		//4. 주소록 정보 추가(PathInfo.add())
+		//5. 주소록 삭제(이름으로 검색 후 삭제 PathInfo.delete())
 		//6. 종료
 		
 		
